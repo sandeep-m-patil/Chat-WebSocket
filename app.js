c9e0208acf5b2a31619a32dab5ab7a8aeb6a9f21
sandeep <sandeeppatil10705@gmail.com>
2025-06-23 15:43:03 +0530

    init: basic chat setup with express and static UI

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 4000;
+const server = app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); });
+
+app.use(express.static(path.join(__dirname, 'public')));
