42c4f95337c01b6d669765ae85640d4c68fb609d
sandeep <sandeeppatil10705@gmail.com>
2025-06-26 17:40:59 +0530

    show messages with time and sound alert

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+
 const PORT = process.env.PORT || 4000;
+
 const server = app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); });
 const io = require('socket.io')(server);
 
@@ -14,16 +16,25 @@ io.on('connection', onConnected);
 
 function onConnected(socket) {
 
-    console.log('A user connected:', socket.id);
+    //console.log('A user connected:', socket.id);
     socketsConnected.add(socket.id);
 
-io.emit('clients-total',socketsConnected.size);
+    io.emit('clients-total', socketsConnected.size);
 
     socket.on('disconnect', () => {
-        console.log('A user disconnected:', socket.id);
+        //console.log('A user disconnected:', socket.id);
         socketsConnected.delete(socket.id);
-io.emit('clients-total',socketsConnected.size);
+        io.emit('clients-total', socketsConnected.size);
 
     });
 
+    socket.on('message', (data) => {
+        //console.log('Message received:', data);
+        socket.broadcast.emit('message', data);
+    })
+
+    socket.on('feedback', (data) => {
+        //console.log('Feedback received:', data);
+        socket.broadcast.emit('feedback', data);
+    })
 }
